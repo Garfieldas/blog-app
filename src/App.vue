70e5788536e7e1fc04e33a4556a36a48276bf37e
Garieldas <dainiusrainys99@gmail.com>
2025-07-13 12:45:00 +0300

    Created sidebar,header overlay components. Added base.css styles and icons

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>Blog app</h1>
+  <SideBar :showNavBar="showNav" @clicked="closeNavBar" />
+  <AppHeader @showNavBar="showNavBar" />
+  <Overlay :showOverlay="showOverlay" @click="closeNavBar" />
+  <Notification />
+  <RouterView />
 </template>
+
+<script setup lang="ts">
+import SideBar from "./components/Layout/SideBar.vue";
+import AppHeader from "./components/Layout/AppHeader.vue";
+import Overlay from "./components/UI/Overlay.vue";
+import { ref } from "vue";
+
+const showNav = ref(false);
+const showOverlay = ref(false);
+
+const showNavBar = () => {
+  showNav.value = true;
+  showOverlay.value = true;
+};
+
+const closeNavBar = () => {
+  showNav.value = false;
+  showOverlay.value = false;
+};
+</script>
---
+++ src/components/Layout/SideBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="sidebar" :class="{'show': showNavBar}">
+  <h2 class="logo">
+    <i class="fa-solid fa-table-columns"></i> Dashboard
+  </h2>
+  <nav>
+    <ul class="nav-group">
+
+      <li @click="$emit('clicked')">
+        <i class="fa-solid fa-envelope"></i> Posts</li>
+
+      <li @click="$emit('clicked')"><i class="fa-regular fa-address-card"></i> Authors</li>
+
+      <li @click="$emit('clicked')"><i class="fa-solid fa-right-to-bracket"></i> Login</li>
+      
+    </ul>
+  </nav>
+</section>
+</template>
+
+<script setup lang="ts">
+const props = defineProps(['showNavBar'])
+</script>
