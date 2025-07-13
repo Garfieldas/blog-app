6f9aeeafb6ba22f7fb105c0664bf0d4b267a0ca6
Garieldas <dainiusrainys99@gmail.com>
2025-07-13 22:01:22 +0300

    created api service. installed json-server@017.4 version. added db.json file. Initialized author card,list and base layout components

---
+++ src/components/UI/Authors/AuthorCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="card">
+        <h4>{{ author.name author.surname }}</h4>
+        <p>Created_at{{ author.created_at }}</p>
+        <div class="buttons">
+        <i class="fa-solid fa-pen-to-square"></i>
+        <i class="fa-solid fa-trash"></i>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps(['author'])
+</script>
---
+++ src/components/UI/Authors/AuthorsList.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="card-wrapper">
+        <AuthorCard  v-for="author in authors" :key="author.id"/>
+    </div>
+</template>
+
+<script setup lang="ts">
+import AuthorCard from './AuthorCard.vue';
+const props = defineProps(['authors']);
+</script>
