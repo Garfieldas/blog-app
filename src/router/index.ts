363a171acc7db35a7a68b7acb79362c0646340f2
Garieldas <dainiusrainys99@gmail.com>
2025-07-13 14:15:03 +0300

    Created routes and error 404 view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: "/",
+      children: [
+        {
+          path: "",
+          name: "posts",
+          component: () => "@/views/PostsView.vue",
+          alias: "/posts",
+        },
+        {
+          path: "posts/:id",
+          name: "postDetails",
+          component: () => "@/views/PostDetails.vue",
+          props: true,
+        },
+      ],
+    },
+    {
+      path: "/authors",
+      name: "authors",
+      component: () => "@/views/AuthorsView.vue",
+    },
+    {
+      path: "/:pathMatch(.*)",
+      name: "NotFound",
+      component: () => "@/views/ErrorView.vue",
+    },
   ],
-})
+});
 
-export default router
+export default router;
---
+++ src/views/ErrorView.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="main">
+        <img src="../assets/icons/error-404.svg" alt="not-found">
+        <h3>Oopss... Page Not Found...</h3>
+    </main>
+</template>
+
+<style scoped>
+.main {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+img {
+  width: 50%;
+  margin: 2em;
+
+}
+</style>
