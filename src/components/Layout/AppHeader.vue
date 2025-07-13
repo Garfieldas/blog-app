2ac7df8089400a21100533ce0bf89d21eddad51c
Dainius <dainiusrainys99@gmail.com>
2025-07-13 12:46:45 +0300

    Created sidebar,header overlay components. Added base.css styles and icons (#1)

---
+++ src/components/Layout/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+<header class="header">
+  <div class="header-left">
+  </div>
+  <div class="header-right">
+    <div class="greeting">
+      <p>Hi there,</p>
+      <strong>John Doe</strong>
+    </div>
+    <img src="../../assets/icons/avatar.png" alt="user avatar" />
+    <button class="nav-button" @click="$emit('showNavBar')">
+    <i class="fas fa-bars"></i>
+  </button>
+  </div>
+</header>
+</template>
