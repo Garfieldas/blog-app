cfb24d465d6784b9631aad3f3115551c415d963f
Dainius R <dainius.rainys@teltonika.lt>
2025-07-14 08:59:45 +0300

    Resolve "create post"

--- src/composables/postService.ts
+++ src/composables/postService.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { baseUrl } from "../../utils/constants";
+import { readStorage } from "../../utils/localStorage";
 
 const api = axios.create({
   baseURL: baseUrl,
@@ -44,5 +45,32 @@ export const getSinglePost = async (id: number) => {
       error: error.message,
       status: false,
     }
+  }
+}
+
+export const createPost = async (title: string, authorId: number, body: string) => {
+  const created_at = new Date().toISOString();
+  const updated_at = new Date().toISOString();
+  const post = { title, authorId, body, created_at, updated_at };
+  const token = readStorage();
+  try {
+      await api.post('/posts', post, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return { status: true }
+  }
+  catch (error: any) {
+    let errorMessage = '';
+
+    if (error.response && error.response.status === 401) {
+      errorMessage = 'Failed to authorize. Please log in again';
+    }
+    else {
+      errorMessage = 'Network failed';
+    }
+
+    return { status: false, error: errorMessage }
   }
 }
